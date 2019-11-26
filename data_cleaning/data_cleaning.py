57d95cad3b57305dd4093a9c3b42d281948bae8b
Natalie Bogda <nbogda@vols.utk.edu>
2019-11-25 23:11:13 -0500

    Added NaN removal and normalization

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -8,20 +8,64 @@ class Data:
 
         self.train = pd.read_csv("%s/TrainingDataset.csv" % data_path)
         self.test = pd.read_csv("%s/TestDataset.csv" % data_path)
+        self.train_rows, self.train_columns = self.train.shape
 
+    #median imputation is for categorical variables
+    def clean_nans(self):
 
+        #replace NaN in Outcome_ with 0 
+        self.train.loc[:, self.train.columns.str.startswith('Outcome_')] = self.train.loc[:, self.train.columns.str.startswith('Outcome_')].fillna(0)
 
+        #check for NaNs in Quan_ columns
+        quan_names = list(self.train.loc[:, self.train.columns.str.startswith('Quan_')])
+        quan_nulls = self.train.loc[:, quan_names].isnull().sum(axis=0)
+        #if more than 60% are null, ICE THAT COLUMN in both train and test
+        quan_nulls /= self.train_rows
+        for i in range(0, len(quan_nulls)):
+            #more than 60% is NaN
+            if quan_nulls[i] > 0.6:
+                self.train.drop(quan_names[i], axis=1, inplace=True)
+                self.test.drop(quan_names[i], axis=1, inplace=True)
+            else:
+                mean = self.train[quan_names[i]].mean()
+                #drop columns with all 0s
+                if mean == 0:
+                    self.train.drop(quan_names[i], axis=1, inplace=True)
+                    self.test.drop(quan_names[i], axis=1, inplace=True)
+                else:
+                    self.train[quan_names[i]] = self.train[quan_names[i]].fillna(mean)
+                    self.test[quan_names[i]] = self.test[quan_names[i]].fillna(mean)
 
+        #fix date columns with NaNs by setting to 0
+        self.train.loc[:, self.train.columns.str.startswith('Date_')] = self.train.loc[:, self.train.columns.str.startswith('Date_')].fillna(0)
+        self.test.loc[:, self.test.columns.str.startswith('Date_')] = self.test.loc[:, self.test.columns.str.startswith('Date_')].fillna(0)
 
+        #checking categorical columns to make sure there is some variance in the column
+        cat_names = list(self.train.loc[:, self.train.columns.str.startswith("Cat_")])
+        for i in range(0, len(cat_names)):
+            #all values are the same, drop the column in both train and test
+            if self.train[cat_names[i]].nunique() == 1:
+                self.train.drop(cat_names[i], axis=1, inplace=True)
+                self.test.drop(cat_names[i], axis=1, inplace=True)
 
-
-
-
-
-
+    
+    def normalize_data(self):
+            
+        col_names = list(self.train.columns)
+        #removing outcomes, dont normalize those
+        col_names[:] = [x for x in col_names if "Outcome" not in x]
+        for i in range(0, len(col_names)):
+            mean = self.train[col_names[i]].mean()
+            std = self.train[col_names[i]].std()
+            self.train[col_names[i]] = (self.train[col_names[i]] - mean)/std
+            self.test[col_names[i]] = (self.test[col_names[i]] - mean)/std
+    
+    
 
 
 if __name__ == "__main__":
 
     #directory that data is located in
     data = Data("../data")
+    data.clean_nans()
+    data.normalize_data()
