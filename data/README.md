d7243fbef3f80852ac8788ec6126521abb7a934d
Natalie Bogda <nbogda@vols.utk.edu>
2019-12-06 01:36:03 -0500

    Made the six datasets, organized dirs, PCA graphs

---
+++ data/README.md
@@ -0,0 +1,13 @@
+# Directory Contains
+
+* clean\_data: Original cleaned csv file before splitting into train and test (70/30)
+
+* NAN\_median: Data that has been cleaned normally (columns with NaN over 60% and columns with all the same value removed, mean imputation for the rest)
+
+* NaN\_deleted: Data where a column with a NaN is obliterated
+
+* NaN\_to\_0: Data where all NaNs replaced with 0
+
+* useless\_data: Speaks for itself
+
+* TrainingDataset.csv: The origin of life (and our data)  
--- data_cleaning/PCA.py
+++ data_cleaning/PCA.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
-def convert_to_csv(x, y, _set):
+def convert_to_csv(x, y, col_name, _set, data):
     #convert split data to pandas dataframe
     tmp_y = np.array(y)
     tmp_x = np.array(x)
     pca_ = np.concatenate((tmp_y, tmp_x), axis=1)
     prediction_labels = ["Outcome %d" % i for i in range(1, tmp_y.shape[1] + 1)]
-    predictor_labels = ["PC %d" % i for i in range(1, tmp_x.shape[1] + 1)]
+    predictor_labels = ["%s %s" % (col_name, i) for i in range(1, tmp_x.shape[1] + 1)]
     labels = np.concatenate((prediction_labels, predictor_labels), axis=0)
     pca_df = pd.DataFrame(pca_, columns=labels)
-    pca_df.to_csv("../data/PCA_split_data_%s.csv" % _set)
+    file_name = "ORIGINAL" if col_name == "Column" else "PCA" 
+    pca_df.to_csv("../data/NAN_%s/%s_split_data_%s.csv" % (data, file_name, _set))
 
 
 if __name__ == "__main__":
 
-    train = pd.read_csv("../data/useless_data/training_set_V1.csv")
+    data = "mean"
+    train = pd.read_csv("../data/cleaned_data/nan_%s.csv" % data)
     col_names = list(train.columns)
     row, columns = train.shape
     
@@ -34,8 +36,9 @@ if __name__ == "__main__":
     #splitting this so we can test our results
     #we don't have ground truth for the test set yet so we will need to use our training data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    convert_to_csv(X_train, y_train, "Column", "train", data)
+    convert_to_csv(X_test, y_test, "Column", "test", data)
 
-    sys.exit(0)
     #create PCA object
     pca = PCA()
     X_train = pca.fit_transform(X_train)
@@ -47,10 +50,10 @@ if __name__ == "__main__":
     #make first plot of just principal components
     fig1 = plt.figure()
     plt.plot(explained_variance)
-    plt.title("Principal Components")
+    plt.title("Principal Components %s" % data)
     plt.ylabel("Percent of Variance Explained")
     plt.xlabel("Principal Component")
-    plt.savefig("principal_comp.png")
+    plt.savefig("PCA_charts/principal_comp_%s.png" % data)
 
     #select what percent var to keep
     desired_var = 0.9
@@ -61,12 +64,12 @@ if __name__ == "__main__":
     #make second plot of cum var explained
     fig2 = plt.figure()
     plt.plot(cumsum)
-    plt.title("Variance Explained")
+    plt.title("Variance Explained %s" % data)
     plt.plot(k, cumsum[k], 'ro', label="Eigenvalue #%d with %.2f Variance" % (k, desired_var))
     plt.legend()
     plt.ylabel("Cumulative Percent of Variance Explained")
     plt.xlabel("Principal Component")
-    plt.savefig("var_exp.png")
+    plt.savefig("PCA_charts/var_exp_%s.png" % data)
             
     #transform the data to lower dimension
     pca = PCA(n_components=int(k))
@@ -74,9 +77,9 @@ if __name__ == "__main__":
     X_test = pca.fit_transform(X_test)
 
     _set = "train"
-    convert_to_csv(X_train, y_train, _set)
+    convert_to_csv(X_train, y_train, "PC", _set, data)
     _set = "test"
-    convert_to_csv(X_test, y_test, _set)
+    convert_to_csv(X_test, y_test, "PC", _set, data)
 
 
     
